f62b48572d2629788f9e4f539a03568fc75fd737
Kevin Charles <char0042@umn.edu>
2020-06-19 15:37:28 -0500

    Working on making a line for graph
    Co-authored-by: davidatherton1 <david.atherton996@gmail.com>
    Co-authored-by: Ryan Greyling <greyl001@umn.edu>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import { Canvas } from 'react-three-fiber'
+import *  as THREE from 'three'
 import Arrow from './Arrow';
 // import { OrbitControls } from 'drei';
 import { MapControls } from 'drei';
 // import { TrackballControls } from 'drei'; //Doesn't flip
 // import { TransformControls } from 'drei';
+//https://spectrum.chat/react-three-fiber/general/basic-example-of-drawing-a-line~9edb471e-0bb2-4814-b346-b2d4829fc78e
 
+var points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
 
 export default () => (
 
       <Canvas>
+        <ambientLight />
         {/* <OrbitControls /> */}
         <MapControls />
+        <mesh>
+          <bufferGeometry setFromPoints = {points}/>
+          <lineBasicMaterial attach="material" color="black"/>
+        </mesh>
         {/* <TrackballControls /> */}
         {/* <TransformControls /> */}
-      <pointLight position={[10, 10, 10]} />
-      <Arrow />
+      {/* <Arrow /> */}
     </Canvas>
 
     )
