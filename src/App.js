3b1989b3bcd9bd395691980b3008fb232a216e61
Kevin Charles <char0042@umn.edu>
2020-07-17 15:37:37 -0500

    Meshline for axis
    Co-authored-by: Ryan Greyling <greyl001@umn.edu>
    Co-authored-by: davidatherton1 <david.atherton996@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Canvas } from 'react-three-fiber'
+import { extend, Canvas } from 'react-three-fiber'
 import *  as THREE from 'three'
 // import Arrow from './Arrow';
 // import { OrbitControls } from 'drei';
@@ -8,93 +8,164 @@ import { MapControls } from 'drei';
 // import { TransformControls } from 'drei';
 //https://spectrum.chat/react-three-fiber/general/basic-example-of-drawing-a-line~9edb471e-0bb2-4814-b346-b2d4829fc78e
 
+import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'threejs-meshline';
 
-function Axis(){
+extend({ MeshLine, MeshLineMaterial })
 
-  var hpoints = [];
-  hpoints.push(new THREE.Vector3(-10, 0, 0));
-  hpoints.push(new THREE.Vector3(10, 0, 0));
-  var vpoints = [];
-  vpoints.push(new THREE.Vector3(0, -10, 0));
-  vpoints.push(new THREE.Vector3(0, 10, 0));
+//To-Do
+//What property of <Canvas/> does scrolling affect? Zoom?
+//Make more gridlines
+//Do we write our own <MapControls> or do we dyanimacally adjust line width?
+
+function Axis() {
+
+  const hvertices = []
+  // for (let j = 0; j < Math.PI; j += (2 * Math.PI) / 100)
+  //   hvertices.push(new THREE.Vector3(Math.cos(j), Math.sin(j), 0))
+  hvertices.push(new THREE.Vector3(-100,0,0));
+  hvertices.push(new THREE.Vector3(100,0,0));
+
+  const vvertices = []
+  
+  vvertices.push(new THREE.Vector3(0,-100,0));
+  vvertices.push(new THREE.Vector3(0,100,0));
+  //   const line = new MeshLine()
+  //   line.setVertices(vertices)
+
+  let width = 0.1;
+  let color = '#403634';
+  //#636160 Color for non-principal axes
   return (<>
-  <mesh >
-          <cylinderBufferGeometry attach="geometry" args={[1,1,2000,32]}/>
-          <meshStandardMaterial attach="material" color="black"/>
-        </mesh>
-    {/* <line>
-    <geometry attach="geometry" vertices={hpoints} />
-    <lineBasicMaterial attach="material" color="black" linewidth={100}/>
-  </line>
-  <line>
-    <geometry attach="geometry" vertices={vpoints} />
-    <lineBasicMaterial attach="material" color="black" linewidth={10} />
-  </line> */}
-  </>
+      <mesh raycast={MeshLineRaycast}>
+        <meshLine attach="geometry" vertices={hvertices} />
+        <meshLineMaterial
+          attach="material"
+          transparent
+          depthTest={false}
+          lineWidth={width}
+          color={color}
+          // dashArray={0.05}
+          // dashRatio={0.95}
+        />
+      </mesh>
+      <mesh raycast={MeshLineRaycast}>
+      <meshLine attach="geometry" vertices={vvertices} />
+      <meshLineMaterial
+        attach="material"
+        transparent
+        depthTest={false}
+        lineWidth={width}
+        color={color}
+        // dashArray={0.05}
+        // dashRatio={0.95}
+      />
+    </mesh>
+    </>
   )
 }
 
 function NewAxis() {
 
-  const offsetToLines = function(offset) {
-    let vpoints = [];
-    vpoints.push(new THREE.Vector3(offset, -10, 0));
-    vpoints.push(new THREE.Vector3(offset, 10, 0));
-    console.log(offset)
-    return (
-      <line key={offset}>
-        <geometry attach="geometry" vertices={vpoints}/>
-        <lineBasicMaterial attach="material" color="black"/>
-      </line>
-    )
-  }
+  const hvertices = []
+  // for (let j = 0; j < Math.PI; j += (2 * Math.PI) / 100)
+  //   hvertices.push(new THREE.Vector3(Math.cos(j), Math.sin(j), 0))
+  hvertices.push(new THREE.Vector3(-100,0,0));
+  hvertices.push(new THREE.Vector3(100,0,0));
 
-  let hpoints = [];
-  hpoints.push(new THREE.Vector3(-10, 0, 0));
-  hpoints.push(new THREE.Vector3(10, 0, 0));
+  const vvertices = []
+  
+  vvertices.push(new THREE.Vector3(0,-100,0));
+  vvertices.push(new THREE.Vector3(0,100,0));
+  //   const line = new MeshLine()
+  //   line.setVertices(vertices)
 
-  let zpoints = [];
-  zpoints.push(new THREE.Vector3(0,0,-10));
-  zpoints.push(new THREE.Vector3(0,0,10));
-
-  let offsets = [-2,-1,0,1,2];
-
-    return (
-      <>
-        <line>
-          <geometry attach="geometry" vertices={hpoints} />
-          <lineBasicMaterial attach="material" color="black" linewidth={100}/>
-        </line>
-        {offsets.map(offsetToLines)}
-        <line>
-          <geometry attach="geometry" vertices={zpoints}/>
-          <lineBasicMaterial attach="material" color="black"/>
-        </line>
-      </>
-    )
-}
-
-function CylinderAxis() {
-  return(
-    <>
+  let width = 0.1;
+  let color = '#403634';
+  return (<>
+      <mesh raycast={MeshLineRaycast}>
+        <meshLine attach="geometry" vertices={hvertices} />
+        <meshLineMaterial
+          attach="material"
+          transparent
+          depthTest={false}
+          lineWidth={width}
+          color={color}
+          // dashArray={0.05}
+          // dashRatio={0.95}
+        />
+      </mesh>
+      <mesh raycast={MeshLineRaycast}>
+      <meshLine attach="geometry" vertices={vvertices} />
+      <meshLineMaterial
+        attach="material"
+        transparent
+        depthTest={false}
+        lineWidth={width}
+        color={color}
+        // dashArray={0.05}
+        // dashRatio={0.95}
+      />
+    </mesh>
     </>
   )
 }
 
+// function MapAxis() {
+
+//   const offsetToLines = function(offset) {
+//     let vpoints = [];
+//     vpoints.push(new THREE.Vector3(offset, -10, 0));
+//     vpoints.push(new THREE.Vector3(offset, 10, 0));
+//     console.log(offset)
+//     return (
+//       <line key={offset}>
+//         <geometry attach="geometry" vertices={vpoints}/>
+//         <lineBasicMaterial attach="material" color="black"/>
+//       </line>
+//     )
+//   }
+
+//   let hpoints = [];
+//   hpoints.push(new THREE.Vector3(-10, 0, 0));
+//   hpoints.push(new THREE.Vector3(10, 0, 0));
+
+//   let zpoints = [];
+//   zpoints.push(new THREE.Vector3(0,0,-10));
+//   zpoints.push(new THREE.Vector3(0,0,10));
+
+//   let offsets = [-2,-1,0,1,2];
+
+//     return (
+//       <>
+//         <line>
+//           <geometry attach="geometry" vertices={hpoints} />
+//           <lineBasicMaterial attach="material" color="black" linewidth={100}/>
+//         </line>
+//         {offsets.map(offsetToLines)}
+//         <line>
+//           <geometry attach="geometry" vertices={zpoints}/>
+//           <lineBasicMaterial attach="material" color="black"/>
+//         </line>
+//       </>
+//     )
+// }
+
+
+
 export default function App() {
   // let [ref, material] = useResource();
-  
-  
+  // let [zoomLevel,setZoomLevel] = useState(50);
+
 
   // const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
 
 
   return (
 
-    <Canvas orthographic camera={{ position: [0, 0, 50], zoom: 10, up: [0, 0, 1], far: 10000 }}>
+    <Canvas orthographic camera={{ position: [0, 0, 2], zoom: 50, up: [0, 0, 1], far: 10000 }}>
       <ambientLight />
       <MapControls />
-      <Axis/>
+      <Axis />
       {/* <OrbitControls /> */}
       {/* <mesh> */}
       {/* <bufferGeometry setFromPoints = {points}/> */}
@@ -108,7 +179,7 @@ export default function App() {
         <lineBasicMaterial attach="material" color="black" />
       </line> */}
 
-     
+
 
     </Canvas>
 
